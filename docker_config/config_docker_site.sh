77c3884e5ec9c0b894e366e5b3547ed07b9ff611
JhoelDiego2 <jhoel.mita@sptech.school>
2025-09-25 22:34:03 -0300

    fix: Dockerfile do node js

--- docker_config/config_docker_site.sh
+++ docker_config/config_docker_site.sh
@@ -8,7 +8,7 @@ APP_CONTAINER_NAME="oberon-web-c"
 APP_PORT="80" 
 
 # Configurações do Build:
-DOCKERFILE_PATH="Docker/site/Dockerfile"
+DOCKERFILE_PATH="Oberon-Config-AWS/Docker/site/Dockerfile" 
 BUILD_CONTEXT="." # O contexto é a raiz do projeto (Oberon-Config-AWS)
 
 print_header() {
@@ -23,20 +23,20 @@ run_web() {
     print_header "SETUP: INICIANDO APLICAÇÃO WEB"
 
     # 1. Limpeza de Containers (Omitido por brevidade no exemplo)
-    
+    echo "-> 1/3: Garantindo containers limpos..."
+    sudo docker stop $DB_CONTAINER_NAME 2> /dev/null
+    sudo docker rm $DB_CONTAINER_NAME 2> /dev/null
     # 2. Build da Imagem
     echo "-> 2/3: Construindo imagem da Aplicação Web: $APP_IMAGE_NAME"
     echo "-> Usando Dockerfile em: $DOCKERFILE_PATH"
     
     # Execução do build: -f aponta para o Dockerfile, e o '.' é o contexto (raiz)
     cd ~
-    sudo docker build \
-        -t $DB_IMAGE_NAME \
-        -f Oberon-Config-AWS/Docker/site/Dockerfile \
-        ./oberon 
+    sudo docker build -t $DB_IMAGE_NAME -f $DOCKERFILE_PATH .
 
     if [ $? -ne 0 ]; then
         echo "ERRO: Falha na construção da imagem da Web. Abortando."
+        cd "$PROJECT_ROOT" 
         return 1
     fi
 
